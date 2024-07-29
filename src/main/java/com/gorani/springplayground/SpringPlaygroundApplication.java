b0ca66e645540fe4437024b58e928e94aea890bd
Curry4182 <kangbk4182@naver.com>
2024-07-29 14:21:11 +0900

    feat: stream 하위 패키지에 있는 메서드 들에 대해 성능 측정하는 aop 추가

--- src/main/java/com/gorani/springplayground/SpringPlaygroundApplication.java
+++ src/main/java/com/gorani/springplayground/SpringPlaygroundApplication.java
@@ -2,8 +2,10 @@ package com.gorani.springplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringPlaygroundApplication {
 
     public static void main(String[] args) {
