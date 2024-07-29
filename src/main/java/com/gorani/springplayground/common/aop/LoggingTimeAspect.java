d5a23538798a2911ec8df7ea7d6395a64e3cb11b
gangbyeong-gon <kangbk41882@gmail.com>
2024-07-29 14:21:11 +0900

    feat: stream 하위 패키지에 있는 메서드 들에 대해 성능 측정하는 aop 추가

---
+++ src/main/java/com/gorani/springplayground/common/aop/LoggingTimeAspect.java
@@ -0,0 +1,26 @@
+package com.gorani.springplayground.common.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingTimeAspect {
+
+    @Around("execution(* com.gorani.springplayground.java.stream.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.nanoTime();
+
+        Object proceed = joinPoint.proceed();
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000; // 밀리초로 변환
+
+        String methodName = joinPoint.getSignature().toShortString();
+        System.out.println("Execution time of " + methodName + " :: " + duration + " ms");
+
+        return proceed;
+    }
+}
