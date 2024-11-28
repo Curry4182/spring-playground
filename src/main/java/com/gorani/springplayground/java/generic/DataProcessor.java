389ca26f7c422f134abbddd2811abba9a5094197
gangbyeong-gon <kangbk41882@gmail.com>
2024-11-28 16:34:24 +0900

    comment: generic wildcard 예제 주석 추가

--- src/main/java/com/gorani/springplayground/java/generic/DataProcessor.java
+++ src/main/java/com/gorani/springplayground/java/generic/DataProcessor.java
@@ -10,16 +10,16 @@ public class DataProcessor<T> {
 
     private final List<T> storage = new ArrayList<>();
 
-    // Covariance, Produce
+    // Covariance
     public void processData(List<? extends T> sensorData) {
-        for (T data : sensorData) {
+        for (T data : sensorData) { // produce - 컬렉션으로부터 원소들을 꺼내면서 T(type parameter) Type 객체를 생성(produce)하고 있다.
             saveData(data);
         }
     }
 
-    // Contravariance, Consumer
+    // Contravariance
     public void addToExternalStorage(List<? super T> externalStorage, T data) {
-        externalStorage.add(data);
+        externalStorage.add(data); // consume - T(type parameter) Type 객체를 소비(consume)하고 있다.
     }
 
     private void saveData(T data) {
